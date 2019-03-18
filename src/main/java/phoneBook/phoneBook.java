fd3d7de86c0a73cef3e26b1fe3e912e3f0bba4fb
tea <teacons@fbear.ru>
2019-03-18 10:00:55 +0300

    Поменял имя класса и пакета для кода и его тестов; Добавил исключения для наличия отсутствия элементов в Map; Изменил доступность методов и полей; Добавил метода отображение телефонной книги; Добавил тесты для новых исключений; Изменил алгоритм проверки неправильных символов.

--- src/main/java/phoneBook/phoneBook.java
+++ src/main/java/phoneBook/phoneBook.java
@@ -1,29 +1,33 @@
-package phoneBook;
+package PhoneBook;
 
 
 import java.util.*;
 
-class phoneBook {
-    static Map<String, List<String>> phoneBookMap = new HashMap<>();
-    static void addPeople(String Name) {
+class PhoneBook {
+    private Map<String, List<String>> phoneBookMap = new HashMap<>();
+    void addPerson(String Name) {
+        if (phoneBookMap.containsKey(Name)) throw new IllegalArgumentException();
         phoneBookMap.put(Name, new ArrayList<>());
     }
-    static void delPeople(String Name) {
+    void delPerson(String Name) {
+        if (!phoneBookMap.containsKey(Name)) throw new IllegalArgumentException();
         phoneBookMap.remove(Name);
     }
-    static void addPhone(String Name, String Phone) {
+    void addPhone(String Name, String Phone) {
         checkWrongSym(Phone);
+        if (!phoneBookMap.containsKey(Name)) throw new IllegalArgumentException();
         List<String> temp = phoneBookMap.get(Name);
         temp.add(Phone);
         phoneBookMap.put(Name, temp);
     }
-    static void delPhone(String Name, String Phone) {
+    void delPhone(String Name, String Phone) {
         checkWrongSym(Phone);
+        if (!phoneBookMap.containsKey(Name)) throw new IllegalArgumentException();
         List<String> temp = phoneBookMap.get(Name);
         temp.remove(Phone);
         phoneBookMap.put(Name, temp);
     }
-    static List<String> searchPhones(String Name) {
+    List<String> searchPhones(String Name) {
         for (Map.Entry<String, List<String>> entry : phoneBookMap.entrySet()) {
             String key = entry.getKey();
             List<String> value = entry.getValue();
@@ -31,7 +35,7 @@ class phoneBook {
         }
         return null;
     }
-    static String searchPeople(String Phone) {
+    String searchPeople(String Phone) {
         checkWrongSym(Phone);
         for (Map.Entry<String, List<String>> entry : phoneBookMap.entrySet()) {
             String key = entry.getKey();
@@ -40,11 +44,10 @@ class phoneBook {
         }
         return null;
     }
-    private static void checkWrongSym(String Phone) {
-        String valStr = "0123456789-+#*";
-        for (int i = 0; i < Phone.length(); i++){
-            char c = Phone.charAt(i);
-            if (valStr.indexOf(c) == -1) throw new IllegalArgumentException();
-        }
+    private void checkWrongSym(String Phone) {
+        if (!Phone.matches("([0-9+\\-*#])+")) throw new IllegalArgumentException();
+    }
+    Map<String, List<String>> phoneBook() {
+        return phoneBookMap;
     }
 }
---
+++ src/test/java/phoneBook/TestPhoneBook.java
@@ -0,0 +1,99 @@
+package PhoneBook;
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class TestPhoneBook {
+    @Test
+    public void addPersonTest() {
+        Map<String, List<String>> testMap = new HashMap<>();
+        testMap.put("Яблоко", new ArrayList<>());
+        PhoneBook test = new PhoneBook();
+        test.addPerson("Яблоко");
+        assertEquals(testMap, test.phoneBook());
+        assertThrows(IllegalArgumentException.class, () -> test.addPerson("Яблоко"));
+    }
+    @Test
+    public void delPersonTest() {
+        Map<String, List<String>> testMap = new HashMap<>();
+        testMap.put("Апельсин", new ArrayList<>());
+        PhoneBook test = new PhoneBook();
+        test.addPerson("Яблоко");
+        test.addPerson("Апельсин");
+        test.delPerson("Яблоко");
+        assertEquals(testMap, test.phoneBook());
+        assertThrows(IllegalArgumentException.class, () -> test.delPerson("Яблоко"));
+    }
+    @Test
+    public void addPhoneTest() {
+        Map<String, List<String>> testMap = new HashMap<>();
+        List<String> testList = new ArrayList<>();
+        testList.add("123456");
+        testMap.put("Апельсин", testList);
+        testMap.put("Яблоко", new ArrayList<>());
+        PhoneBook test = new PhoneBook();
+        test.addPerson("Яблоко");
+        test.addPerson("Апельсин");
+        test.addPhone("Апельсин", "123456");
+        assertEquals(testMap, test.phoneBook());
+        assertThrows(IllegalArgumentException.class, () -> test.addPhone("Апельсин", "dsfgbs"));
+        assertThrows(IllegalArgumentException.class, () -> test.addPhone("Груша", "123456"));
+    }
+    @Test
+    public void delPhoneTest() {
+        Map<String, List<String>> testMap = new HashMap<>();
+        List<String> testList = new ArrayList<>();
+        testList.add("123456");
+        testMap.put("Апельсин", new ArrayList<>());
+        testMap.put("Яблоко", new ArrayList<>());
+        PhoneBook test = new PhoneBook();
+        test.addPerson("Яблоко");
+        test.addPerson("Апельсин");
+        test.addPhone("Апельсин", "123456");
+        test.delPhone("Апельсин", "123456");
+        assertEquals(testMap, test.phoneBook());
+        assertThrows(IllegalArgumentException.class, () -> test.delPhone("Апельсин", "dsfgbs"));
+    }
+    @Test
+    public void searchPhonesTest() {
+        List<String> testList = new ArrayList<>();
+        testList.add("123456");
+        List<String> test2List = new ArrayList<>();
+        test2List.add("123");
+        test2List.add("123456789");
+        PhoneBook test = new PhoneBook();
+        test.addPerson("Яблоко");
+        test.addPerson("Апельсин");
+        test.addPhone("Апельсин", "123456");
+        test.addPerson("Мандарин");
+        test.addPhone("Мандарин", "123");
+        test.addPhone("Мандарин", "123456789");
+        assertEquals(testList, test.searchPhones("Апельсин"));
+        assertEquals(test2List, test.searchPhones("Мандарин"));
+        assertNull(test.searchPhones("Груша"));
+        assertEquals(new ArrayList<String>(), test.searchPhones("Яблоко"));
+    }
+    @Test
+    public void searchPeopleTest() {
+        List<String> testList = new ArrayList<>();
+        testList.add("123456");
+        List<String> test2List = new ArrayList<>();
+        test2List.add("123456789");
+        test2List.add("123");
+        PhoneBook test = new PhoneBook();
+        test.addPerson("Яблоко");
+        test.addPhone("Яблоко", "123456789");
+        test.addPhone("Яблоко", "123");
+        test.addPerson("Апельсин");
+        test.addPhone("Апельсин", "123456");
+        assertEquals("Апельсин", test.searchPeople("123456"));
+        assertEquals("Яблоко", test.searchPeople("123456789"));
+        assertEquals("Яблоко", test.searchPeople("123"));
+        assertNull(test.searchPeople("762354"));
+        assertThrows(IllegalArgumentException.class, () -> test.searchPeople("dsfgbs"));
+    }
+}
